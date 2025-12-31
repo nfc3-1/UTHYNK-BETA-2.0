2826e12808434573ee8ed574d7e76c2af8bff874
nfc3-1 <nick.catrambone@yahoo.com>
2025-12-30 19:09:06 -0600

    Delete app directory

--- app/daily/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-import { getTodayChallengeId } from '@/lib/daily';
-
-export default function Daily(){
-  const id = getTodayChallengeId();
-  redirect(`/challenge/${id}`);
-}
