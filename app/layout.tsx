b18b64e55e0c74ad156bf3477848d45224a516c6
nfc3-1 <nick.catrambone@yahoo.com>
2026-01-09 21:36:01 -0600

    Reorder attributes in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 // app/layout.tsx
 import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-screen w-screen bg-uthynk text-white overflow-x-hidden">
+      <body className="min-h-screen w-screen overflow-x-hidden bg-uthynk text-white">
         {children}
       </body>
     </html>
   );
 }
-
