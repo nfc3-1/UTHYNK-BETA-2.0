b499a5c738863893f7a3c75e48984fc629646d31
nfc3-1 <nick.catrambone@yahoo.com>
2026-01-17 15:36:12 -0600

    Simplify body element in RootLayout
    Removed specific classes from the body element.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
+// app/layout.tsx
 import "./styles/globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-screen w-screen overflow-x-hidden bg-uthynk text-white">
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
+
